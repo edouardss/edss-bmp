c6517f4cf38e4cad1b8e0332e32f395a808b78ec
edouardss <edouardss@gmail.com>
2025-09-15 15:41:16 +0100

    module to Module

--- src/main.py
+++ src/main.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     # Register the model with the module
     # module = Module()
     # module.add_model(BmpSensor.MODEL, BmpSensor)
-    asyncio.run(module.run_from_registry())
+    asyncio.run(Module.run_from_registry())
